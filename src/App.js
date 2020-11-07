ca49865a77c669bcd86ac13e0aecab417a49aafa
Molajack <wer12800@gmail.com>
2020-11-07 11:31:49 +0800

    Implement React lazy loading with Suspense

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, Suspense, lazy } from 'react';
 import { Route, Switch } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 //custom components
 import Loading from "./components/Loading";
-import Main from "./Main";
-import Answer from "./Answer"
 //style
 import './style/App.scss';
 import './style/normalize.css';
+
+
+const Main = lazy(() => import('./Main'));
+const Answer = lazy(() => import('./Answer'));
 
 const App = () => {
   const [selectPrice, setSelectPrice] = useState(null);
@@ -17,20 +19,22 @@ const App = () => {
       <Loading />
       <Route
         render={({ location }) => (
-          <AnimatePresence exitBeforeEnter>
-            <Switch location={location} key={location.pathname}>
-              <Route
-                exact
-                path='/'
-                render={() => <Main setSelectPrice={setSelectPrice} />}
-              />
-              <Route
-                exact
-                path='/answer'
-                render={() => <Answer selectPrice={selectPrice} />}
-              />
-            </Switch>
-          </AnimatePresence>
+          <Suspense fallback={null}>
+            <AnimatePresence exitBeforeEnter>
+              <Switch location={location} key={location.pathname}>
+                <Route
+                  exact
+                  path='/'
+                  render={() => <Main setSelectPrice={setSelectPrice} />}
+                />
+                <Route
+                  exact
+                  path='/answer'
+                  render={() => <Answer selectPrice={selectPrice} />}
+                />
+              </Switch>
+            </AnimatePresence>
+          </Suspense>
         )}
       />
       <div className="jar-tsai">ⓒ2020 GinNaLiJarTsai</div>
